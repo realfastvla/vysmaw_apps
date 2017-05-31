ab4d3244a892aa997a2f11fd3ddfdd253944f8dd
Casey Law <caseyjlaw@gmail.com>
2017-05-31 13:20:10 -0600

    works with 70k msg/s and 1 second of data in 4 seconds of processing

--- timefilter.pyx
+++ timefilter.pyx
@@ -93,19 +93,21 @@ cpdef filter1(t0, t1, nant=3, nspw=1, nchan=64, npol=1, inttime_micros=1000000, 
     cdef unsigned int nspec = ni*nbl*nspw*npol
     cdef long starttime = time.time()
     cdef long currenttime = starttime
+    cdef unsigned int frac
+    cdef bool printed = 0
 
     cdef np.ndarray[np.int_t, ndim=1, mode="c"] antarr = np.array([ant for ant in np.arange(1, nant+1+len(excludeants)) if ant not in excludeants]) # 1 based
-    cdef np.ndarray[np.int_t, ndim=2, mode="c"] blarr = np.array([(antarr[ind0], antarr[ind1]) for ind1 in range(len(antarr)) for ind0 in range(0, ind1)])
+#    cdef np.ndarray[np.int_t, ndim=2, mode="c"] blarr = np.array([(antarr[ind0], antarr[ind1]) for ind1 in range(len(antarr)) for ind0 in range(0, ind1)])
+    cdef list blarr = ['{0}-{1}'.format(antarr[ind0], antarr[ind1]) for ind1 in range(len(antarr)) for ind0 in range(0, ind1)]
     cdef np.ndarray[np.float64_t, ndim=1, mode="c"] timearr = t0+(inttime_micros/1e6)*(np.arange(ni)+0.5)
-#    cdef np.ndarray[np.complex128_t, ndim=4, mode="c"] data = np.zeros(shape=(ni, nbl, nchantot, npol), dtype='complex128')
-
-    cdef np.ndarray[np.complex128_t, ndim=3, mode="c"] data = np.zeros(shape=(nspec, nchan, npol), dtype='complex128')
+    cdef np.ndarray[np.complex128_t, ndim=4, mode="c"] data = np.zeros(shape=(ni, nbl, nchantot, npol), dtype='complex128')
+#    cdef np.ndarray[np.complex128_t, ndim=3, mode="c"] data = np.zeros(shape=(nspec, nchan, npol), dtype='complex128')
 
     print('Expecting {0} ints, {1} bls, and {2} total spectra between times {3} and {4} (timeout {5} s)'.format(ni, nbl, nspec, t0, t1, timeout))
 
     # count until total number of spec is received or timeout elapses
     cdef long spec = 0
-    while ((msg is NULL) or (msg[0].typ is not VYSMAW_MESSAGE_END)) and (spec < nspec):# and (currenttime - starttime < timeout):
+    while ((msg is NULL) or (msg[0].typ is not VYSMAW_MESSAGE_END)) and (spec < nspec) and (currenttime - starttime < timeout):
         msg = vysmaw_message_queue_timeout_pop(queue0, 100000)
 
         if msg is not NULL:
@@ -113,29 +115,29 @@ cpdef filter1(t0, t1, nant=3, nspw=1, nchan=64, npol=1, inttime_micros=1000000, 
             if msg[0].typ is VYSMAW_MESSAGE_VALID_BUFFER:
                 py_msg = Message.wrap(msg)
 
+
                 # get the goodies asap
                 msg_time = py_msg.info.timestamp/1e9
-                ch0 = nchan*py_msg.info.baseband_index  # TODO: need to be smarter here
+                ch0 = nchan*py_msg.info.baseband_index  # ** TODO: need to be smarter here
                 pind = py_msg.info.polarization_product_id
-                stations = np.array(py_msg.info.stations)
+#                print('ch0, pind: {0}, {1}'.format(ch0, pind))
+                blstr = '{0}-{1}'.format(py_msg.info.stations[0], py_msg.info.stations[1])
                 spectrum = np.array(py_msg.spectrum, copy=True)  # ** slow, but helps to pull data early and unref
                 py_msg.unref()
 
-#                print(msg_time, ch0, pind, stations)
                 # TODO: may be smarter to define acceptable data from input parameters here. drop those that don't fit?
-#                hasstations = [np.all(bl == stations) for bl in blarr]  # way too slow
-#                print('has baseline {0}'.format(stations))
+                if blstr in blarr:
+#                    print('has baseline {0}'.format(blstr))
 
-#                bind = np.where(hasstations)[0][0]
-#                iind = np.argmin(np.abs(timearr-msg_time))
+                    bind = blarr.index(blstr)
+#                    print('bl: {0}, {1}'.format(blstr, bind))
+                    iind = np.argmin(np.abs(timearr-msg_time))
+#                    print('time: {0}, {1}'.format(msg_time, iind))
 
-#                data[iind, bind, ch0:ch0+nchan, pind].real = np.array(py_msg.spectrum)[::2]
-#                data[iind, bind, ch0:ch0+nchan, pind].imag = np.array(py_msg.spectrum)[1::2]
+                    data[iind, bind, ch0:ch0+nchan, pind].real = spectrum[::2] # slow
+                    data[iind, bind, ch0:ch0+nchan, pind].imag = spectrum[1::2] # slow
 
-                data[spec, :, pind].real = spectrum[::2] # ** slow
-                data[spec, :, pind].imag = spectrum[1::2] # ** slow
-
-                spec = spec + 1
+                    spec = spec + 1
 
             else:
                 print(str('uh oh: {0}'.format(message_types[msg[0].typ])))
@@ -144,8 +146,13 @@ cpdef filter1(t0, t1, nant=3, nspw=1, nchan=64, npol=1, inttime_micros=1000000, 
 #            print('NULL')
 
         currenttime = time.time()
-        if not spec % 1000:
-            print('At spec {0}: {1} % of data in {2}x realtime'.format(spec, 100*float(spec)/float(nspec), (currenttime-starttime)/(t1-t0)))
+        frac = int(100.*spec/nspec)
+        if not (frac % 25):
+            if not printed:
+                print('At spec {0}: {1:1.0f} % of data in {2:1.1f}x realtime'.format(spec, 100*float(spec)/float(nspec), (currenttime-starttime)/(t1-t0)))
+                printed = 1
+        else:
+            printed = 0
 
         PyErr_CheckSignals()
 
