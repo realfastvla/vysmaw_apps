57ef31595ae3537cd1c16347796245158f210d01
Casey Law <caseyjlaw@gmail.com>
2019-02-12 15:35:44 -0700

    adding small shift from middle of integration

--- vysmaw_reader.pyx
+++ vysmaw_reader.pyx
@@ -259,7 +259,7 @@ cdef class Reader(object):
 
         # fill arrays
         for iind in range(self.ni):
-            timearr[iind] = self.t0+(self.inttime_micros/1000000)*(iind+0.5)  # TODO: shift from center to avoid multiple assignment?
+            timearr[iind] = self.t0+(self.inttime_micros/1000000)*(iind+0.501)
 
         for ind1 in range(self.nant):
             for ind0 in range(ind1):
