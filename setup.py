17e27d2ea84d56218f6c5c3bf037dacb56b3860f
Casey Law <caseyjlaw@gmail.com>
2019-05-23 10:51:26 -0600

    numpy.get_include in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
+import numpy
 
 extensions = [
     Extension("*", ["*.pyx"],
         libraries = ["vysmaw", "vys", "python3.6m"],
 # standard build
-        include_dirs = ["/opt/cbe-local/include", "."],
+        include_dirs = ["/opt/cbe-local/include", ".", numpy.get_include()],
         library_dirs = ["/home/cbe-master/wcbe/py/lib/python3.6/site-packages", "/opt/cbe-local/lib"],
 # development build
 #        include_dirs = ["/users/mpokorny/projects/vysmaw/src", "."],
